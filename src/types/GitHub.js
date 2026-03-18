1b7eecdcbd6d053dd9a349f514fe5ddd122554ae
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-03-19 00:43:49 +0100

    chore: things

---
+++ src/types/GitHub.js
@@ -0,0 +1,23 @@
+/**
+ * @typedef {object} GitHubIssue
+ *
+ * @property {string | null} body
+ * @property {string} title
+ * @property {url} string;
+ */
+
+/**
+ * @typedef {object} GitHubRepository
+ *
+ * @property {string} fullName
+ * @property {string} name
+ * @property {string} url
+ */
+
+/**
+ * @typedef {import("@octokit/openapi-types").components['schemas']['issue']} RawGithubIssue
+ */
+
+/**
+ * @typedef {import("@octokit/openapi-types").components['schemas']['repository']} RawGithubRepository
+ */
