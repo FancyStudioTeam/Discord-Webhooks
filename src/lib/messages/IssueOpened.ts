71b4992a0afcf25e317c4822631d51e9369d08d2
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-03-18 23:27:48 +0100

    feat: add `ISSUE_OPENED_MESSAGE`

---
+++ src/lib/messages/IssueOpened.ts
@@ -0,0 +1,43 @@
+import type { GitHubIssue, GitHubRepository } from "#/types/GitHub";
+import {
+	ContainerBuilder,
+	heading,
+	HeadingLevel,
+	TextDisplayBuilder,
+} from "@discordjs/builders";
+import { ISSUE_OPENED_EMOJI } from "../Emojis.js";
+import { formatRepositoryHyperlink } from "#/utils/formatRepositoryHyperlink.js";
+import { GREEN_COLOR } from "../Colors.js";
+
+export function ISSUE_OPENED_MESSAGE({
+	issue,
+	repository,
+}: IssueOpenedMessageOptions): ContainerBuilder {
+	const { title: issueTitle } = issue;
+	const { fullName: repositoryFullName, url: repositoryUrl } = repository;
+
+	const repositoryHyperlink = formatRepositoryHyperlink(
+		repositoryFullName,
+		repositoryUrl,
+	);
+
+	const containerBuilder = new ContainerBuilder();
+	const containerTitleBuilder = new TextDisplayBuilder();
+
+	containerTitleBuilder.setContent(
+		heading(
+			`${ISSUE_OPENED_EMOJI} ${repositoryHyperlink} Issue Opened: ${issueTitle}`,
+			HeadingLevel.Three,
+		),
+	);
+
+	containerBuilder.addTextDisplayComponents(containerTitleBuilder);
+	containerBuilder.setAccentColor(GREEN_COLOR);
+
+	return containerBuilder;
+}
+
+interface IssueOpenedMessageOptions {
+	issue: GitHubIssue;
+	repository: GitHubRepository;
+}
